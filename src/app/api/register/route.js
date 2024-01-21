bdeed1a30c8bc891bae387ab8e10611c92415101
Mehmed Muric <mehmedmuric22@gmail.com>
2024-01-22 00:49:03 +0100

    mongoose UserSchema

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,3 +1,10 @@
-export function POST(req) {
+import mongoose from "mongoose";
+import { User } from "../../models/user";
+
+export async function POST(req) {
+    const body = await req.json();
+    mongoose.connect(process.env.MONGO_URL);
+    const createUser = await User.create(body);
     
+    return Response.json(createUser);
 }
