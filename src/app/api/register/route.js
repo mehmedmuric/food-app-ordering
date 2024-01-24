99ad6c02dcc8adab8fdb58014b3973ba4a99871a
Mehmed Muric <mehmedmuric22@gmail.com>
2024-01-24 23:57:15 +0100

    add bcrypt for hashed pass

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -4,7 +4,7 @@ import { User } from "../../models/user";
 export async function POST(req) {
     const body = await req.json();
     mongoose.connect(process.env.MONGO_URL);
-    const createUser = await User.create(body);
+    const createdUser = await User.create(body);
     
-    return Response.json(createUser);
+    return Response.json(createdUser);
 }
--- src/app/models/user.js
+++ src/app/models/user.js
@@ -1,4 +1,5 @@
-import { Schema } from "mongoose";
+import bcrypt from 'bcrypt';
+import { model, models, Schema } from "mongoose";
 
 const UserSchema = new Schema({
     email: {type: String, required: true, unique: true},
@@ -8,10 +9,15 @@ const UserSchema = new Schema({
         validate: pass => {
             if(!pass?.length || pass.length < 5){
                 new Error('password must be at least 5 characters!');
-                return false;
             }
         }, 
     },
 }, {timestamps: true});
+
+UserSchema.post('validate', function (user) {
+    const notHashedPass = user.password;
+    const salt = bcrypt.genSaltSync(10);
+    user.password = bcrypt.hashSync(notHashedPass, salt);
+});
 
 export const User = models?.User || model('User', UserSchema);
