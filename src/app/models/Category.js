b95079c1f9c1931ac091f1a5e7e00e73035dc4a9
Mehmed Muric <mehmedmuric22@gmail.com>
2024-02-12 21:52:35 +0100

    admin category uodate

---
+++ src/app/models/Category.js
@@ -0,0 +1,8 @@
+import { Schema, models, model } from "mongoose";
+
+const CategorySchema = new Schema({
+    name: {type: String, required: true},
+}, {timestamps: true});
+
+
+export const Category = models?.Category || model('Category', CategorySchema);
